747855b7681424ba0383c91cafa14e6ac0aafe2a
Emiliyan Kadiyski <e.kadiyski@gmail.com>
2023-03-30 12:17:51 +0300

    Make influxdb connection more robust
    Use Asynchronous non-blocking API - WriteApi  https://github.com/influxdata/influxdb-client-java/tree/master/client#asynchronous-non-blocking-api

--- src/main/java/de/wejago/hichi2influx/service/SensorMessageSubscriber.java
+++ src/main/java/de/wejago/hichi2influx/service/SensorMessageSubscriber.java
@@ -1,10 +1,12 @@
 package de.wejago.hichi2influx.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
 import de.wejago.hichi2influx.dto.SensorEntry;
 import de.wejago.hichi2influx.repository.InfluxDbRepository;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
@@ -14,21 +16,35 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-    public class SensorMessageSubscriber implements IMqttMessageListener {
-        private final ObjectMapper objectMapper;
+public class SensorMessageSubscriber implements IMqttMessageListener {
+    private final ObjectMapper objectMapper;
+    private final InfluxDbRepository influxDbRepository;
 
-        private final InfluxDbRepository influxDbRepository;
-
-        @Override public void messageArrived(String s, MqttMessage mqttMessage) {
-            try {
-                String receivedMessage = new String(mqttMessage.getPayload());
-                log.info("Received message: " + receivedMessage);
-                if(receivedMessage.contains("1_8_0")) {
-                    SensorEntry sensorEntry = objectMapper.readValue(receivedMessage, SensorEntry.class);
-                    influxDbRepository.writePoint(sensorEntry);
-                }
-            } catch (JsonProcessingException e) {
-                log.error("objectMapper JsonProcessingException" + e);
+    @Override public void messageArrived(String s, MqttMessage mqttMessage) {
+        try {
+            String receivedMessage = new String(mqttMessage.getPayload());
+            log.info("Received message: " + receivedMessage);
+            if (receivedMessage.contains("1_8_0")) {
+                SensorEntry sensorEntry = objectMapper.readValue(receivedMessage, SensorEntry.class);
+                Point point = generateMeasurementPoint(sensorEntry);
+                influxDbRepository.writePoint(point);
             }
+        } catch (JsonProcessingException e) {
+            log.error("objectMapper JsonProcessingException" + e);
         }
     }
+
+    private static Point generateMeasurementPoint(SensorEntry sensorEntry) {
+        return Point.measurement("sensor")
+                    .addTag("sensor_id", sensorEntry.getSml().getDeviceId())
+                    .addField("totalConsumption(1_8_0)", sensorEntry.getSml().getTotalConsumption())
+                    .addField("tariff1Consumption(1_8_1)", sensorEntry.getSml().getTariff1Consumption())
+                    .addField("tariff2Consumption(1_8_2)", sensorEntry.getSml().getTariff2Consumption())
+                    .addField("energyExport(2_8_0)", sensorEntry.getSml().getEnergyExport())
+                    .addField("currentConsumption(16_7_0)", sensorEntry.getSml().getCurrentConsumption())
+                    .addField("currentConsumptionPhase1(36_7_0)", sensorEntry.getSml().getCurrentConsumptionPhase1())
+                    .addField("currentConsumptionPhase2(56_7_0)", sensorEntry.getSml().getCurrentConsumptionPhase2())
+                    .addField("currentConsumptionPhase3(76_7_0)", sensorEntry.getSml().getCurrentConsumptionPhase3())
+                    .time(Instant.now(), WritePrecision.MS);
+    }
+}
---
+++ src/test/java/de/wejago/hichi2influx/service/SensorMessageSubscriberTest.java
@@ -0,0 +1,82 @@
+package de.wejago.hichi2influx.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.wejago.hichi2influx.dto.SensorEntry;
+import de.wejago.hichi2influx.dto.SensorMeasurement;
+import de.wejago.hichi2influx.repository.InfluxDbRepository;
+import de.wejago.hichi2influx.service.SensorMessageSubscriber;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SensorMessageSubscriberTest {
+    private final String TIME_AS_STRING = "2022-02-16 02:25:32";
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private InfluxDbRepository influxDbRepository;
+
+    @InjectMocks
+    private SensorMessageSubscriber subscriber;
+
+    @Test
+    void messageArrived_withValidMessage_writesPointToInfluxDbRepository() throws Exception {
+        // GIVEN
+        String receivedMessage = "{\"Time\":\"2023-02-12T21:29:36\",\"SML\":{\"1_8_0\":22417.98}}".trim();
+        SensorEntry expectedSensorEntry = generateTestSensorEntry();
+        when(objectMapper.readValue(receivedMessage, SensorEntry.class)).thenReturn(expectedSensorEntry);
+
+        // WHEN
+        subscriber.messageArrived("test", new MqttMessage(receivedMessage.getBytes()));
+
+        // THEN
+        verify(influxDbRepository, times(1)).writePoint(any());
+    }
+
+    @Test
+    void messageArrived_withJsonMissingSensorField_ignoresMessage() throws Exception {
+        // GIVEN
+        String receivedMessage = "{\"value\":10}";
+
+        // WHEN
+        subscriber.messageArrived("test", new MqttMessage(receivedMessage.getBytes()));
+
+        // THEN
+        verifyNoInteractions(influxDbRepository);
+    }
+
+    private SensorEntry generateTestSensorEntry() {
+        SensorEntry testSensorEntry = new SensorEntry();
+        testSensorEntry.setTime(LocalDateTime.parse(TIME_AS_STRING, formatter).atOffset(ZoneOffset.UTC).toString());
+        SensorMeasurement sensorMeasurement = new SensorMeasurement();
+        sensorMeasurement.setTotalConsumption(100.0);
+        sensorMeasurement.setTariff1Consumption(50.0);
+        sensorMeasurement.setTariff2Consumption(30.0);
+        sensorMeasurement.setEnergyExport(20.0);
+        sensorMeasurement.setCurrentConsumption(10.0);
+        sensorMeasurement.setCurrentConsumptionPhase1(5.0);
+        sensorMeasurement.setCurrentConsumptionPhase2(3.0);
+        sensorMeasurement.setCurrentConsumptionPhase3(2.0);
+        sensorMeasurement.setDeviceId("myDevice");
+        testSensorEntry.setSml(sensorMeasurement);
+
+        return testSensorEntry;
+    }
+}
